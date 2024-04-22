5095de037f143882f60a3a9f1cca528ea2e7856d
Sunzheini <daniel_zorov@abv.bg>
2024-04-22 13:16:12 +0300

    Added placeholder views

--- ohmi_audit/main_app/views.py
+++ ohmi_audit/main_app/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'index.html')
+
+
+def about_view(request):
+    return render(request, 'index.html')
+
+
+def contact_view(request):
+    return render(request, 'index.html')
+
+
+def login_view(request):
+    return render(request, 'index.html')
+
+
+def register_view(request):
+    return render(request, 'index.html')
+
+
+def logout_view(request):
+    return render(request, 'index.html')
