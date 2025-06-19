895327e5c623f122b06f2faa42566dc021368115
Sunzheini <daniel_zorov@abv.bg>
2025-06-19 14:52:31 +0300

    Successful form save

--- common/common_models_data.py
+++ common/common_models_data.py
@@ -99,6 +99,7 @@ class CustomModelBase(models.Model):
                     + '-'
                     + self.full_name.lower().replace(' ', '-')
             )
+        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         """
