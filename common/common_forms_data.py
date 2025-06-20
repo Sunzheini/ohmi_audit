cf0e5048b10ebc5fab17576a28931a0e30279f2c
Sunzheini <daniel_zorov@abv.bg>
2025-06-20 09:33:26 +0300

    Working on forms

--- common/common_forms_data.py
+++ common/common_forms_data.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.utils import translation
 
+__all__ = ['ChangeLabelsMixin', 'FormWidgetStylesMixin']
+
 
 class ChangeLabelsMixin:
     def change_to_given_labels(self, labels: dict = None):
@@ -24,3 +26,101 @@ class ChangeLabelsMixin:
         for field_name, label in labels.items():
             if field_name in self.fields:
                 self.fields[field_name].label = label
+
+
+class FormWidgetStylesMixin:
+    def set_widget_styles(self):
+        """
+        Applies consistent Bootstrap styling to all form fields.
+        - Date fields get HTML5 date picker
+        - Boolean fields get proper checkbox styling
+        - Choice fields get select styling
+        - Text fields get appropriate controls
+        """
+        for field_name, field in self.fields.items():
+            # Skip already processed fields
+            if hasattr(field, '_widget_processed'):
+                continue
+
+            # DateField (override widget completely)
+            if isinstance(field, forms.DateField):
+                field.widget = forms.DateInput(attrs={
+                    'type': 'date',
+                    'class': 'form-control'
+                })
+                field._widget_processed = True
+                continue  # Skip remaining checks for this field
+
+            # BooleanField (checkboxes)
+            elif isinstance(field, forms.BooleanField):
+                field.widget = forms.CheckboxInput(attrs={
+                    'class': 'form-check-input'
+                })
+                field._widget_processed = True
+                continue
+
+            # Choice fields (Select/SelectMultiple)
+            elif isinstance(field, (forms.ChoiceField, forms.ModelChoiceField, forms.ModelMultipleChoiceField)):
+                field.widget.attrs.update({'class': 'form-select'})
+                field._widget_processed = True
+                continue
+
+            # CharField
+            if isinstance(field.widget, forms.TextInput):
+                field.widget.attrs.update({
+                    'class': 'form-control',
+                    'placeholder': field.label
+                })
+
+            # CharField with choices (Select)
+            elif isinstance(field.widget, forms.Select):
+                field.widget.attrs.update({
+                    'class': 'form-select',
+                    'placeholder': field.label
+                })
+
+            # TextField
+            elif isinstance(field.widget, forms.Textarea):
+                field.widget.attrs.update({
+                    'class': 'form-control',
+                    'rows': 2,
+                    'placeholder': field.label
+                })
+
+            # FileField
+            elif isinstance(field.widget, forms.ClearableFileInput):
+                field.widget.attrs.update({
+                    'class': 'form-control-file',
+                    'placeholder': field.label
+                })
+
+            # EmailField
+            elif isinstance(field.widget, forms.EmailInput):
+                field.widget.attrs.update({
+                    'class': 'form-control',
+                    'placeholder': field.label
+                })
+
+            # URLField
+            elif isinstance(field.widget, forms.URLInput):
+                field.widget.attrs.update({
+                    'class': 'form-control',
+                    'placeholder': field.label
+                })
+
+            # NumberField
+            elif isinstance(field.widget, forms.NumberInput):
+                field.widget.attrs.update({
+                    'class': 'form-control',
+                    'placeholder': field.label
+                })
+
+            # Other fields (default to form-control)
+            else:
+                field.widget.attrs.update({
+                    'class': 'form-control',
+                    'placeholder': field.label
+                })
+
+            # Mark widget as processed to avoid reapplying styles
+            field._widget_processed = True
--- ohmi_audit/main_app/forms.py
+++ ohmi_audit/main_app/forms.py
@@ -1,18 +1,16 @@
 from django import forms
 
-from common.common_forms_data import ChangeLabelsMixin
+from common.common_forms_data import *
 from ohmi_audit.main_app.models import *
 
 __all__ = ['AuditForm']
 
 
-class AuditForm(forms.ModelForm, ChangeLabelsMixin):
+class AuditForm(forms.ModelForm, ChangeLabelsMixin, FormWidgetStylesMixin):
     class Meta:
         model = Audit
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self.fields['date'].widget = forms.DateInput(attrs={'type': 'date'})
-        self.fields['description'].widget = forms.Textarea(attrs={'rows': 3})
+        self.set_widget_styles()
