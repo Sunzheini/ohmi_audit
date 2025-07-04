3d120cfad73a927407056d4cb0188072c49b5dcb
Sunzheini <daniel_zorov@abv.bg>
2025-07-04 17:04:32 +0300

    db adjustments

--- ohmi_audit/settings.py
+++ ohmi_audit/settings.py
@@ -187,22 +187,24 @@ WSGI_APPLICATION = 'ohmi_audit.wsgi.application'
 # }
 
 # Local PostgreSQL database settings
-DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE'),
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     }
+# }
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default'] = dj_database_url.config(
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600,
         conn_health_checks=True,
     )
+}
 
 # Render.com /w postgresql
 # DATABASES = {
