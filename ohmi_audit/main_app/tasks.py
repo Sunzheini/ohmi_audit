16724e92f9ec4f9f4a58242744d04786a66bd48f
Sunzheini <daniel_zorov@abv.bg>
2025-07-11 11:09:05 +0300

    displayed the update

--- ohmi_audit/main_app/tasks.py
+++ ohmi_audit/main_app/tasks.py
@@ -1,9 +1,6 @@
 import time
 from celery import shared_task
 
-from django.core.mail import send_mail
-from django.conf import settings
-
 
 @shared_task(bind=True)
 def long_running_task(self, duration=5):
@@ -12,16 +9,3 @@ def long_running_task(self, duration=5):
         time.sleep(1)
         self.update_state(state='PROGRESS', meta={'current': i, 'total': duration})
     return f"Completed {duration} second task"
-
-
-@shared_task
-def send_email_task(subject, message, recipient):
-    """Task to send emails asynchronously"""
-    send_mail(
-        subject,
-        message,
-        settings.DEFAULT_FROM_EMAIL,
-        [recipient],
-        fail_silently=False,
-    )
-    return f"Email sent to {recipient}"
